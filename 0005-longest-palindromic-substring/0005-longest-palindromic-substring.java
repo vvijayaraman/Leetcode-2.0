8644b7e43bc74e6df517bd2f189d90198e1af667
Vasudev Vijayaraman <35582753+vvijayaraman0822@users.noreply.github.com>
2024-11-11 23:09:43 -0500

    Time: 16 ms (70.96%), Space: 44.8 MB (55.7%) - LeetHub

---
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
@@ -0,0 +1,34 @@
+class Solution {
+
+    String longestStr = "";
+
+    public String longestPalindrome(String s) {
+        // 1. Start from the centre and loop outside
+        // 2. You could have 2 centres, odd and even so handle both cases.
+
+        for(int i = 0; i < s.length(); i++) {
+
+            findPalindrome(i, i, s); // Left and right start in the centre
+            findPalindrome(i, i+1, s); // Left and right start next to each other, 2 middles
+        }
+
+        return longestStr;
+        
+    }
+
+    public void findPalindrome(int left, int right, String s) {
+        String currStr = "";
+
+        while(left >= 0 && right < s.length() && s.charAt(left) == s.charAt(right)) {
+            left--;
+            right++;
+        }
+
+        currStr = s.substring(left + 1, right);
+
+        if (currStr.length() > longestStr.length()) {
+            longestStr = currStr;
+        }
+
+    }
+}
