de901a4f78c5eb2d502d96b8a39e4270a0738ca0
Vasudev Vijayaraman <35582753+vvijayaraman0822@users.noreply.github.com>
2024-11-11 23:12:40 -0500

    Time: 16 ms (70.96%), Space: 45.7 MB (18.45%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
@@ -1,26 +1,35 @@
+// TC - O(N^2)
+// SC - O(n) - string created
 class Solution {
 
-    String longest = "";
+    String longestStr = "";
 
     public String longestPalindrome(String s) {
-        for (int i = 0; i < s.length(); i++) {
-            longestPalindrome(s, i, i);
-            longestPalindrome(s, i, i+1);
+        // 1. Start from the centre and loop outside
+        // 2. You could have 2 centres, odd and even so handle both cases.
+
+        for(int i = 0; i < s.length(); i++) {
+
+            findPalindrome(i, i, s); // Left and right start in the centre
+            findPalindrome(i, i+1, s); // Left and right start next to each other, 2 middles
         }
 
-        return longest;
+        return longestStr;
         
     }
 
-    public void longestPalindrome(String s, int left, int right) {
-        while(left >= 0 && right <= s.length() - 1 && s.charAt(left) == s.charAt(right)) {
+    public void findPalindrome(int left, int right, String s) {
+        String currStr = "";
+
+        while(left >= 0 && right < s.length() && s.charAt(left) == s.charAt(right)) {
             left--;
             right++;
         }
 
-        String currString = s.substring(left + 1, right);
-        if (currString.length() > longest.length()) {
-            longest = currString;
+        currStr = s.substring(left + 1, right);
+
+        if (currStr.length() > longestStr.length()) {
+            longestStr = currStr;
         }
 
     }
