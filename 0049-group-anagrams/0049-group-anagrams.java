6e750e09b230463d5e471a2f9954a70e91eb8fba
Vasudev Vijayaraman <35582753+vvijayaraman0822@users.noreply.github.com>
2025-01-20 19:52:40 -0500

    Time: 6 ms (97.99%), Space: 47.7 MB (88.5%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.java
+++ 0049-group-anagrams/0049-group-anagrams.java
@@ -1,35 +1,47 @@
 class Solution {
     public List<List<String>> groupAnagrams(String[] strs) {
+        Map<Integer, List<String>> map = new HashMap<>();
         List<List<String>> list = new ArrayList<>();
 
-        Map<Integer, List<String>> map = new HashMap<>();
+        if (strs.length == 1) {
+            List<String> iList = new ArrayList<>();
+            iList.add(strs[0]);
 
-        for(int i = 0; i < strs.length; i++) {
-            Integer hash = buildHash(strs[i]);
+            list.add(iList);
+            return list;
+        }
 
+
+        for(String st : strs) {
+            int[] arr = new int[26];
+            for (char ch : st.toCharArray()) {
+                arr[ch - 'a']++;
+            }
+
+            int hash = buildHash(arr);
             if (map.containsKey(hash)) {
-                map.get(hash).add(strs[i]);
+                map.get(hash).add(st);
             } else {
-                List<String> lt = new ArrayList<>();
-                lt.add(strs[i]);
-                map.put(hash, lt);
+                List<String> iList = new ArrayList<>();
+                iList.add(st);
+
+                map.put(hash, iList);
             }
             
-            
         }
 
-        list.addAll(map.values());
-
+        for (int key : map.keySet()) {
+            List<String> iList = map.get(key);
+            list.add(iList);
+        }
+        
         return list;
+        
     }
 
-    public Integer buildHash(String str) {
-        int[] arr = new int[26];
-        for (char ch : str.toCharArray()) {
-            int ascii = ch - 'a';
-            arr[ascii]++;
-        }
-
+    public int buildHash(int[] arr) {
         return Arrays.hashCode(arr);
+
+
     }
 }
