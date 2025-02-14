b624117f83bfb706d0f4a8da26adb5271b805dcd
Vasudev Vijayaraman <35582753+vvijayaraman0822@users.noreply.github.com>
2025-02-13 23:50:00 -0500

    Time: 97 ms (10.53%), Space: 56.2 MB (95.53%) - LeetHub

--- 0945-minimum-increment-to-make-array-unique/0945-minimum-increment-to-make-array-unique.java
+++ 0945-minimum-increment-to-make-array-unique/0945-minimum-increment-to-make-array-unique.java
@@ -1,22 +1,37 @@
 class Solution {
     public int minIncrementForUnique(int[] nums) {
-        // [3,2,1,2,1,7]
-        // [1,1,2,2,3,7]
-        // [1,2,3,4,5,7]
-        
-        Arrays.sort(nums);
+        Map<Integer, Integer> map = new HashMap<>();
+
+        int max = 0;
+
+        for(int num : nums) {
+            max = Math.max(max, num);
+            map.put(num, map.getOrDefault(num, 0) + 1);
+        }
+
         int moves = 0;
 
-        for(int i = 1; i < nums.length; i++) {
-            if (nums[i] <= nums[i-1]) {
-                int difference = nums[i-1] - nums[i];
-                nums[i] = difference + 1 + nums[i];
-                moves += difference + 1;
+        // The iterations are always n + maximum value
+        // Example [7,7,7,7,7,7,7]
+        // [7,8,9,10,11,12,13] so you'll need the loop to run from o - 13 so you don't miss iterating the end
+
+        for(int i = 0; i < nums.length + max; i++) {
+            if (!map.containsKey(i)) {
+                continue;
             }
 
+            if (map.get(i) > 1) { // carry over the rest to the next value
+                int carry = map.get(i) - 1; // keep 1 and carry over rest
+                map.put(i+1, map.getOrDefault(i+1, 0) + carry);
+                moves += carry;
+
+            } 
+          
         }
 
         return moves;
+
+
         
     }
 }
