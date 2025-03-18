e7b91ed06c846ecd60088200fd1773fb04617031
Vasudev Vijayaraman <35582753+vvijayaraman0822@users.noreply.github.com>
2025-03-18 10:25:11 -0400

    Time: 7 ms (68.33%), Space: 45 MB (31.67%) - LeetHub

--- 0723-candy-crush/0723-candy-crush.java
+++ 0723-candy-crush/0723-candy-crush.java
@@ -1,10 +1,11 @@
 class Solution {
     public int[][] candyCrush(int[][] board) {
+        if (board == null || board.length == 0) {
+            return board;
+        }
 
-        // Tag elements that are repeating and could be crushed + Apply the gravity logic once crushed
+        boolean isDone = true;
 
-        // 1 - Iterate all rows and tag the element
-        boolean done = true;
         for(int i = 0; i < board.length; i++) {
             for(int j = 0; j < board[0].length - 2; j++) {
                 int num1 = Math.abs(board[i][j]);
@@ -15,12 +16,11 @@ class Solution {
                     board[i][j] = -num1;
                     board[i][j+1] = -num2;
                     board[i][j+2] = -num3;
-                    done = false;
+                    isDone = false;
                 }
             }
         }
-         
-        // 2 - Iterate all cols and tag the element
+
          for(int i = 0; i < board.length - 2; i++) {
             for(int j = 0; j < board[0].length; j++) {
                 int num1 = Math.abs(board[i][j]);
@@ -31,38 +31,38 @@ class Solution {
                     board[i][j] = -num1;
                     board[i+1][j] = -num2;
                     board[i+2][j] = -num3;
-                    done = false;
+                    isDone = false;
                 }
-            } 
+            }
+        }
+
+        // Apply gravity logic
+        if (!isDone) {
+        for(int c = 0; c < board[0].length; c++) {
+            int index = board.length - 1;
+
+            for(int r = board.length - 1; r >=0; r--) {
+                if (board[r][c] > 0) {
+                    board[index][c] = board[r][c];
+                    index--;
+                }
+
+            }
+
+            for(int i = index; i >= 0; i--) {
+                board[i][c] = 0;
+                
+            }
+        }
         }
 
 
-
-    
-          // Apply gravity logic for all zeroes
-          if (!done) {
-            for(int c = 0; c < board[0].length; c++) {
-                int index = board.length - 1;
-
-                for(int r = board.length - 1; r >= 0; r--) {
-                    if (board[r][c] > 0) {
-                        board[index][c] = board[r][c];
-                        index--;
-                }
-            }
-
-                for(int r = index; r >= 0; r--) {
-                    board[r][c] = 0;
-                }
-            }
-          }
-           
-
-        if (done) {
+        if (isDone) {
             return board;
         } else {
             return candyCrush(board);
         }
+
         
     }
 }
