af91430f8c7a2686a28b891e83169154ce740d17
Vasudev Vijayaraman <35582753+vvijayaraman0822@users.noreply.github.com>
2025-03-20 09:46:35 -0400

    Time: 7 ms (67.97%), Space: 44.8 MB (77.44%) - LeetHub

--- 0723-candy-crush/0723-candy-crush.java
+++ 0723-candy-crush/0723-candy-crush.java
@@ -1,11 +1,9 @@
 class Solution {
     public int[][] candyCrush(int[][] board) {
-        if (board == null || board.length == 0) {
-            return board;
-        }
 
-        boolean isDone = true;
+        boolean done = true;
 
+        // Tag the column
         for(int i = 0; i < board.length; i++) {
             for(int j = 0; j < board[0].length - 2; j++) {
                 int num1 = Math.abs(board[i][j]);
@@ -13,56 +11,68 @@ class Solution {
                 int num3 = Math.abs(board[i][j+2]);
 
                 if (num1 == num2 && num2 == num3 && num1 != 0) {
-                    board[i][j] = -num1;
+                    board[i][j] = - num1;
                     board[i][j+1] = -num2;
                     board[i][j+2] = -num3;
-                    isDone = false;
+                    done = false;
+
                 }
             }
         }
 
-         for(int i = 0; i < board.length - 2; i++) {
+        // Tag the row
+        for(int i = 0; i < board.length - 2; i++) {
             for(int j = 0; j < board[0].length; j++) {
                 int num1 = Math.abs(board[i][j]);
                 int num2 = Math.abs(board[i+1][j]);
                 int num3 = Math.abs(board[i+2][j]);
 
                 if (num1 == num2 && num2 == num3 && num1 != 0) {
-                    board[i][j] = -num1;
+                    board[i][j] = - num1;
                     board[i+1][j] = -num2;
                     board[i+2][j] = -num3;
-                    isDone = false;
+                    done = false;
+
                 }
             }
         }
 
-        // Apply gravity logic
-        if (!isDone) {
-        for(int c = 0; c < board[0].length; c++) {
-            int index = board.length - 1;
-
-            for(int r = board.length - 1; r >=0; r--) {
-                if (board[r][c] > 0) {
-                    board[index][c] = board[r][c];
-                    index--;
+        // if negative, destroy and mark to 0
+        for(int i = 0; i < board.length; i++) {
+            for(int j = 0; j < board[0].length; j++) {
+                if (board[i][j] < 0) {
+                    board[i][j] = 0;
+                    done = false;
                 }
-
-            }
-
-            for(int i = index; i >= 0; i--) {
-                board[i][c] = 0;
                 
             }
         }
+
+        // Gravity logic
+        if (!done) {
+            for(int c = 0; c < board[0].length; c++) {
+                int index = board.length - 1;
+                for(int r = board.length - 1; r >= 0; r--) {
+                    // if positive, push it down
+                    if (board[r][c] > 0) {
+                        board[index][c] = board[r][c];
+                        index--;
+                }
+            }
+
+            // mark the rest as zeroes
+                for(int i = index; i >= 0; i--) {
+                    board[i][c] = 0;
+                }
+            }
         }
 
 
-        if (isDone) {
+        if (done) {
             return board;
         } else {
             return candyCrush(board);
-        }
-
+        } 
         
     }
 }
