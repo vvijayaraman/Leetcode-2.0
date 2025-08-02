81157c89d15527282d20f1328f18f686208b5810
Vasudev Vijayaraman <35582753+vvijayaraman@users.noreply.github.com>
2025-08-02 22:56:53 +0100

    Time: 7 ms (82.16%), Space: 46 MB (66.5%) - LeetHub

--- 0253-meeting-rooms-ii/0253-meeting-rooms-ii.java
+++ 0253-meeting-rooms-ii/0253-meeting-rooms-ii.java
@@ -1,24 +1,30 @@
 class Solution {
     public int minMeetingRooms(int[][] intervals) {
-        Arrays.sort(intervals, (a,b) -> a[0] - b[0]); // sort by start time
-        int answer = 0;
+        Arrays.sort(intervals, (a,b) -> a[0] - b[0]); // sort by start date
+        int rooms = 0;
+        PriorityQueue<int[]> minHeap = new PriorityQueue<>(
+            (a,b) -> a[1] - b[1]
+        );
 
-        Queue<int[]> queue = new PriorityQueue<>((a,b) -> a[1] - b[1]);
-        queue.add(intervals[0]);
+        minHeap.add(intervals[0]);
+        rooms++;
 
-        for (int i = 1; i < intervals.length; i++) {
-                int[] arr = queue.remove();
-
-                // does it overlap;
-                if (intervals[i][0] < arr[1]) {
-                    queue.add(intervals[i]);
-                    queue.add(arr);
-                } else {
-                    queue.add(intervals[i]);
-                }
+        for(int i = 1; i < intervals.length; i++) {
+            // see if mine starts before the latest one ends
+            if (intervals[i][0] < minHeap.peek()[1]) {
+                rooms++;
+            } else {
+                minHeap.remove();
             }
 
-        return queue.size();
-        
+            minHeap.add(intervals[i]);
+
+
+        }
+
+        return rooms;
+
+
+
     }
 }
