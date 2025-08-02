59d77f120c1f0fc499ea1b62e450fe078b6dc2d5
Vasudev Vijayaraman <35582753+vvijayaraman@users.noreply.github.com>
2025-08-02 19:09:06 +0100

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.java
+++ 0056-merge-intervals/0056-merge-intervals.java
@@ -1,50 +1,33 @@
 class Solution {
     public int[][] merge(int[][] intervals) {
-        Arrays.sort(intervals, (a,b) -> a[0] - b[0]);
-
-        // Use a stack to keep track of pevious intervals
-        Stack<Pair> stack = new Stack<>();
-        stack.push(new Pair(intervals[0][0], intervals[0][1]));
+        Stack<Pair<Integer, Integer>> stack = new Stack<>();
+        stack.push(new Pair<>(intervals[0][0], intervals[0][1]));
 
         for(int i = 1; i < intervals.length; i++) {
-            Pair pair = stack.pop(); // prev interval
-
-            if (pair.right >= intervals[i][0]) { // overlapping
-                Pair newPair = new Pair(pair.left, intervals[i][1]);
-                stack.push(newPair);
+            // need to merge
+            if (intervals[i][0] <= stack.peek().getValue()) {
+                Pair<Integer, Integer>  pop = stack.pop();
+                int left = Math.min(intervals[i][0], pop.getKey());
+                int right = Math.max(intervals[i][1], pop.getValue());
+                stack.push(new Pair<>(left, right));
             } else {
-                stack.push(pair);
-                stack.push(new Pair(intervals[i][0], intervals[i][1]));
+                stack.push(new Pair<>(intervals[i][0], intervals[i][1]));
+
             }
         }
 
-        int[][] ans = new int[stack.size()][2];
-        int pointer = stack.size() - 1;
+        int[][] arr = new int[stack.size()][2];
+        int pointer = arr.length - 1;
 
         while(!stack.isEmpty()) {
-            Pair pr = stack.pop();
-            int[] arr = new int[2];
-            arr[0] = pr.left;
-            arr[1] = pr.right;
-
-            ans[pointer] = arr;
+            Pair<Integer, Integer> pair = stack.pop();
+            arr[pointer][0] = pair.getKey();
+            arr[pointer][1] = pair.getValue();
             pointer--;
         }
 
-        return ans;
+        return arr;
 
         
     }
 }
-
-class Pair {
-    public int left;
-    public int right;
-
-    public Pair(int left, int right) {
-        this.left = left;
-        this.right = right;
-    }
-
-}
-
